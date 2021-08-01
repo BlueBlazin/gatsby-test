9821b374b931c1feff366df9b52289225ad7b59d
blueblazin <sanjeet.dasharath@gmail.com>
2021-08-01 15:56:40 +0530

    tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "gatsby";
+
+function AboutPage() {
+  return (
+    <div>
+      <div>About Page</div>
+      <Link to="/">Home Page</Link>
+    </div>
+  );
+}
+
+export default AboutPage;
